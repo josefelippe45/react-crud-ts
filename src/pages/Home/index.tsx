182e0fe3e3e5d6e2d44e10bc853992f5cb9578ac
José Felippe Gonçalves Oliveira <jose.oliveira370@etec.sp.gov.br>
2021-05-03 15:14:53 -0300

    feat: fetching users at home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,48 @@
-import React, { FC } from 'react';
-import { Container } from './styles';
+import React, { FC, useEffect, useState } from 'react';
+import { Container, CardContainer, Card } from './styles';
+import api from 'services';
+import { AxiosResponse } from 'axios';
+import Cookies from 'js-cookie';
+
+interface IUsers {
+  name: string;
+  password: string;
+  email: string;
+}
 
 const Home: FC = () => {
+  const [users, setUsers] = useState<AxiosResponse<IUsers[]>>();
+  const [error, setError] = useState('');
+  const getUsers = async () => {
+    try {
+      const users: AxiosResponse<IUsers[]> = await api.get('/users', {
+        headers: { auth_token: Cookies.get('auth-token') },
+      });
+      return setUsers(users);
+    } catch (error) {
+      console.error(error);
+      setError('Users not found :(');
+      setUsers(users);
+    }
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
   return (
     <Container>
-      <h1>You're logged</h1>
+      {error && <div>{error}</div>}
+      <CardContainer>
+        <h1>List of users</h1>
+        {users?.data.map((user, i) => (
+          <>
+            <p>User: {i + 1}</p>
+            <Card>
+              <p>Name: {user.name}</p>
+              <p>Email: {user.email}</p>
+            </Card>
+          </>
+        ))}
+      </CardContainer>
     </Container>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -7,3 +7,33 @@ export const Container = styled.div`
   background: white;
   height: calc(100vh - 67px);
 `;
+export const CardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  background: ${(p) => p.theme.colors.primary};
+  color: white;
+  width: 367px;
+  border-radius: 8px;
+  > p {
+    width: 65%;
+  }
+  > h1 {
+    font-size: 22px;
+    margin: 30px 0;
+  }
+  > div:last-child {
+    margin-bottom: 30px;
+  }
+`;
+
+export const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+  border: 2px solid white;
+  border-radius: 10px;
+  width: 65%;
+  margin: 20px 0;
+`;
